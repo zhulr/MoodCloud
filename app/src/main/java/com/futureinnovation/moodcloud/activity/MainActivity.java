c0e9c34a304408cdbbf8392d523f502cdce110d9
zhulr <git>
2015-08-27 09:16:04 +0800

    项目创建

---
+++ app/src/main/java/com/futureinnovation/moodcloud/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.futureinnovation.moodcloud.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.futureinnovation.moodcloud.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/futureinnovation/moodcloud/activity/PersonalCenterActivity.java
@@ -0,0 +1,10 @@
+package com.futureinnovation.moodcloud.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by zhulr on 2015/8/24.
+ */
+public class PersonalCenterActivity extends Activity {
+
+}
