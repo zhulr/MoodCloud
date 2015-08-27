53af1feb71d89e932543c1b6569f315f31f30a54
zhulr <git>
2015-08-27 15:31:51 +0800

    个人中心添加

--- app/src/main/java/com/futureinnovation/moodcloud/activity/MainActivity.java
+++ app/src/main/java/com/futureinnovation/moodcloud/activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.futureinnovation.moodcloud.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.futureinnovation.moodcloud.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    Button myCenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        myCenter = (Button) findViewById(R.id.my_center);
+        myCenter.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this,PersonalCenterActivity.class));
     }
 }
--- app/src/main/java/com/futureinnovation/moodcloud/activity/PersonalCenterActivity.java
+++ app/src/main/java/com/futureinnovation/moodcloud/activity/PersonalCenterActivity.java
@@ -1,10 +1,17 @@
 package com.futureinnovation.moodcloud.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.futureinnovation.moodcloud.R;
 
 /**
  * Created by zhulr on 2015/8/24.
  */
 public class PersonalCenterActivity extends Activity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_center);
+    }
 }
